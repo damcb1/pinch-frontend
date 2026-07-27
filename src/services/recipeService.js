bf764ec211cab81263c07f415a92c93f186edce2
Damaris Castro <damaris.icastro@gmail.com>
2026-07-27 15:33:55 +0200

    feat: add search and filters to my recipes page

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -19,7 +19,15 @@ export const deleteRecipe = async (id) => {
     await api.delete(`/recipes/${id}`);
 };
 
-export const getMyRecipes = async () => {
-    const response = await api.get('/recipes');
+export const getMyRecipes = async (filters = {})=> {
+    const params = {};
+    if (filters.q) params.q = filters.q;
+    if (filters.cuisine) params.cuisine = filters.cuisine;
+    if (filters.minTime) params.minTime = filters.minTime;
+    if (filters.maxTime) params.maxTime = filters.maxTime;
+    if (filters.origin) params.origin = filters.origin;
+    if (filters.difficulty) params.difficulty = filters.difficulty;
+
+    const response = await api.get('/recipes', {params});
     return response.data;
 }
